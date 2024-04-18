c28ace9e36099a4eb9c7fa3802150828bbcef0c4
root <root@TZRAILINGS.COM>
2024-04-18 03:12:22 -0700

    First edit

--- script2.sh
+++ script2.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-echo "file added from Local"
-
-echo "File edited form Local"
-
-echo "File edited form GitHub web"
-
-echo "File edited from 'git branch --show-current' Branch"
+echo "file added first time"
