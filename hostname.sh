ea7ec0bb1aeaad045fd1804bec2f25bf48327d14
root <root@ip-172-31-82-187.ec2.internal>
2024-05-01 16:12:21 +0000

    hostname.sh file created

---
+++ hostname.sh
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+     stages {
+        stage('To get the Hostname')
+        {
+            steps {
+                script {
+                    def servername = sh(script: 'uname -n', returnStdout: true).trim()
+                    echo "The hostname of the server is: ${servername}"
+                }
+            }
+        }
+    
+    
+        stage('To get the Hostname of Node1 machine') {
+            agent {label'Node1'}
+            steps {
+                script {
+                    def servername = sh(script: 'uname -n', returnStdout: true).trim()
+                    echo "The hostname of the server is: ${servername}"
+                }
+            }
+        }
+        stage('To get the Hostname of Node2 machine') {
+            agent {label'Node2'}
+            steps {
+                script {
+                    def servername = sh(script: 'uname -n', returnStdout: true).trim()
+                    echo "The hostname of the server is: ${servername}"
+                }
+            }
+        }
+    }
+}
+
+
+
+
