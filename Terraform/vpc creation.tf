5f2c8a333008a8062674ed5018e45efa1ffeff9c
Govind-Rasu <99677947+Govind-Rasu@users.noreply.github.com>
2024-05-06 08:07:33 +0530

    Created vpc creation.tf

---
+++ Terraform/vpc creation.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region  = "us-east-1"
+  access_key = "AKIA4XJ36Q5QTA7JP2HW"
+  secret_key = "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"
+}
+
+    resource "aws_vpc" "VPC-Auto" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "VPC-Auto"
+  }
+}
+
+resource "aws_subnet" "Subnet-Public" {
+  vpc_id     = aws_vpc.VPC-Auto.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Subnet-Public"
+  }
+}
+
+resource "aws_subnet" "Subnet-Private" {
+  vpc_id     = aws_vpc.VPC-Auto.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Subnet-Private"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.VPC-Auto.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+
+# Create the main route table for the VPC
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.VPC-Auto.id
+   tags = {
+    Name = "main"
+  }
+}
+
+# Associate the private subnet with the main route table
+resource "aws_route_table_association" "assoprivate" {
+  subnet_id      = aws_subnet.Subnet-Private.id
+  route_table_id = aws_route_table.main.id
+
+ 
+}
+
+# Create a custom route table for the public subnet with an internet gateway
+resource "aws_route_table" "custom" {
+  vpc_id = aws_vpc.VPC-Auto.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+   tags = {
+    Name = "custom"
+  }
+}
+
+# Associate the public subnet with the custom route table
+resource "aws_route_table_association" "assopublic" {
+  subnet_id      = aws_subnet.Subnet-Public.id
+  route_table_id = aws_route_table.custom.id
+}
